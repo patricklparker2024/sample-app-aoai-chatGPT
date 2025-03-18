74912c80f7c7adb4de0de91945d35d9918d4fc35
patricklparker2024 <pparker@sprucelawllc.com>
2025-03-17 21:57:21 -0400

    Update vite-env.d.ts

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_UI_TITLE: string;
+  readonly VITE_UI_LOGO: string;
+  readonly VITE_UI_CHAT_TITLE: string;
+  readonly VITE_UI_CHAT_DESCRIPTION: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
